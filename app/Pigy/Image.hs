aaf1cc8fdcfa624fd5c262b41aecba2d448edfdb
Brian W Bush <code@functionally.io>
2021-07-04 20:58:40 -0600

    added scalability of pigy parts

--- app/Pigy/Image.hs
+++ app/Pigy/Image.hs
@@ -5,12 +5,21 @@ module Pigy.Image (
 
 import Codec.Picture(PixelRGBA8(..), writePng)
 import Codec.Picture.Types (Image)
-import Graphics.Rasterific (Cap(..), Drawing, Join(..), Texture, V2(..), circle, cubicBezierFromPath, fill, line, renderDrawing, roundedRectangle, stroke, withClipping, withTexture)
+import Graphics.Rasterific (Cap(..), Drawing, Join(..), Texture, V2(..), circle, cubicBezierFromPath, fill, line, renderDrawing, roundedRectangle, stroke, withClipping, withTexture, withTransformation)
 import Graphics.Rasterific.Texture (uniformTexture)
+import Graphics.Rasterific.Transformations (scale, translate)
 
 
 test :: IO ()
-test = writePng "yourimage.png" pigyImage
+test =
+  writePng "yourimage.png"
+    $ pigyImage
+      1       -- aspect ratio
+      (1, 1)  -- head scale
+      (1, 1)  -- eye scale
+      (1, 1)  -- nose scale
+      (1, 1)  -- ear scale
+      1       -- torso scale
 
 
 white :: Texture PixelRGBA8
@@ -29,33 +38,68 @@ height :: Float
 height = 287
 
 
-pigyImage :: Image PixelRGBA8
-pigyImage =
+withAspect :: Float
+           -> (Float, Float)
+           -> Drawing px ()
+           -> Drawing px ()
+withAspect aspect =
+  withScale
+    (
+      minimum [1,     aspect]
+    , minimum [1, 1 / aspect]
+    )
+
+
+withScale :: (Float, Float)
+          -> (Float, Float)
+          -> Drawing px ()
+          -> Drawing px ()
+withScale (sx, sy) (cx, cy) =
+  withTransformation
+    $  translate (V2    cx     cy )
+    <> scale sx sy
+    <> translate (V2 (- cx) (- cy))
+
+
+pigyImage :: Float
+          -> (Float, Float)
+          -> (Float, Float)
+          -> (Float, Float)
+          -> (Float, Float)
+          -> Float
+          -> Image PixelRGBA8
+pigyImage aspect headScale eyeScale noseScale earScale bodyScale =
   renderDrawing (round width) (round height) (PixelRGBA8 0xFF 0xFF 0xFF 0x00)
+    . withAspect aspect (width / 2, height / 2)
     $ do
       let
-        pink1 = uniformTexture $ PixelRGBA8 0xff 0x57 0xa7 0xff
-        pink2 = uniformTexture $ PixelRGBA8 0xff 0x85 0xc0 0xff
-        pink3 = uniformTexture $ PixelRGBA8 0xff 0x70 0xb5 0xff
-        pink4 = uniformTexture $ PixelRGBA8 0xff 0x41 0x9c 0xff
-        pink5 = uniformTexture $ PixelRGBA8 0xff 0xad 0xd4 0xff
-      drawBody pink1 pink2 pink1
-      drawHead pink3 pink4
-      drawEyes black white
-      drawEars pink2 pink1
-      drawNose pink5 pink4 pink3 black
+        pink1 = uniformTexture $ PixelRGBA8 0xFF 0x57 0xA7 0xFF
+        pink2 = uniformTexture $ PixelRGBA8 0xFF 0x85 0xC0 0xFF
+        pink3 = uniformTexture $ PixelRGBA8 0xFF 0x70 0xB5 0xFF
+        pink4 = uniformTexture $ PixelRGBA8 0xFF 0x41 0x9C 0xFF
+        pink5 = uniformTexture $ PixelRGBA8 0xFF 0xAD 0xD4 0xFF
+      drawBody bodyScale pink1 pink2 pink1
+      withScale headScale (width / 2, 150)
+        $ do
+          drawHead pink3 pink4
+          drawEyes eyeScale black white
+          drawEars earScale pink2 pink1
+          withScale noseScale (width / 2, 125)
+            $ drawNose pink5 pink4 pink3 black
 
 
-drawBody :: Texture px
+drawBody :: Float
+         -> Texture px
          -> Texture px
          -> Texture px
          -> Drawing px ()
-drawBody torsoColor bellyColor bottomColor =
+drawBody bodyScale torsoColor bellyColor bottomColor =
   do
     withTexture bottomColor
       . fill
       $ roundedRectangle (V2 16.163944 215.98409) 212.15131 99.693665 55 55
-    withTexture  torsoColor
+    withScale (bodyScale, 1) (width / 2, 215)
+      . withTexture  torsoColor
       . fill
       $ roundedRectangle (V2 58.40379 139.59184) 126.36129 153.8943 80 80
     withTexture bellyColor
@@ -77,33 +121,41 @@ drawHead frontColor backColor =
       $ roundedRectangle (V2 20.827299 34.955734) 201.86699 116.59785 60 60
 
 
-drawEyes :: Texture px
+drawEyes :: (Float, Float)
+         -> Texture px
          -> Texture px
          -> Drawing px ()
-drawEyes eyeColor pupilColor =
+drawEyes eyeScale eyeColor pupilColor =
   do
-    withTexture eyeColor
-      $ mapM_ fill
-      [
-        circle (V2 75.04925 101.45342) 9.72327
-      , circle (V2 (width - 75.04925) 101.45342) 9.72327
-      ]
-    withTexture pupilColor
-      $ mapM_ fill
-      [
-        circle (V2 70.61203 97.36941) 3.64652
-      , circle (V2 (width - 70.61203) 97.36941) 3.64652
-      ]
+    withScale eyeScale (75, 100)
+      $ do
+        withTexture eyeColor
+          . fill
+          $ circle (V2 75.04925 101.45342) 9.72327
+        withTexture pupilColor
+          . fill
+          $ circle (V2 70.61203 97.36941) 3.64652
+    withScale eyeScale (170, 100)
+      $ do
+        withTexture eyeColor
+          . fill
+          $ circle (V2 (width - 75.04925) 101.45342) 9.72327
+        withTexture pupilColor
+          . fill
+          $ circle (V2 (width - 70.61203) 97.36941) 3.64652
 
 
-drawEars :: Texture px
+drawEars :: (Float, Float)
+         -> Texture px
          -> Texture px
          -> Drawing px ()
-drawEars frontColor backColor =
+drawEars earScale frontColor backColor =
   do
-    withTexture backColor
-      $ mapM_ (fill . cubicBezierFromPath)
-        [
+    withScale earScale (54, 47)
+      $ do
+        withTexture backColor
+          . fill 
+          $ cubicBezierFromPath
           [
             V2 0          0
           , V2 34.875935  0.42684743
@@ -116,7 +168,27 @@ drawEars frontColor backColor =
           , V2 31.658189 11.934829
           , V2 0          0
           ]
-        , [
+        withTexture frontColor
+          . fill
+          $ cubicBezierFromPath
+          [       
+            V2 0          0
+          , V2 50.861558 15.800834
+          , V2 38.191333 57.31195
+          , V2 37.471204 58.134955
+          , V2 33.553602 63.778565
+          , V2 30.631682 69.593209
+          , V2 27.302137 75.122339
+          , V2 14.99146  52.777337
+          , V2 18.687946 21.667265
+          , V2 0          0
+          ]
+    withScale earScale (187, 47)
+      $ do
+        withTexture backColor
+          . fill 
+          $ cubicBezierFromPath
+          [
             V2  width               0
           , V2 (width - 34.875935)  0.42684743
           , V2 (width - 69.101494) 15.066973
@@ -128,35 +200,21 @@ drawEars frontColor backColor =
           , V2 (width - 31.658189) 11.934829
           , V2  width               0
           ]
-        ]
-    withTexture frontColor
-      $ mapM_ (fill . cubicBezierFromPath)
-      [
-        [       
-          V2 0          0
-        , V2 50.861558 15.800834
-        , V2 38.191333 57.31195
-        , V2 37.471204 58.134955
-        , V2 33.553602 63.778565
-        , V2 30.631682 69.593209
-        , V2 27.302137 75.122339
-        , V2 14.99146  52.777337
-        , V2 18.687946 21.667265
-        , V2 0          0
-        ]
-      , [       
-          V2  width               0
-        , V2 (width - 50.861558) 15.800834
-        , V2 (width - 38.191333) 57.31195
-        , V2 (width - 37.471204) 58.134955
-        , V2 (width - 33.553602) 63.778565
-        , V2 (width - 30.631682) 69.593209
-        , V2 (width - 27.302137) 75.122339
-        , V2 (width - 14.99146 ) 52.777337
-        , V2 (width - 18.687946) 21.667265
-        , V2  width               0
-        ]
-      ]
+        withTexture frontColor
+          . fill 
+          $ cubicBezierFromPath
+          [       
+            V2  width               0
+          , V2 (width - 50.861558) 15.800834
+          , V2 (width - 38.191333) 57.31195
+          , V2 (width - 37.471204) 58.134955
+          , V2 (width - 33.553602) 63.778565
+          , V2 (width - 30.631682) 69.593209
+          , V2 (width - 27.302137) 75.122339
+          , V2 (width - 14.99146 ) 52.777337
+          , V2 (width - 18.687946) 21.667265
+          , V2  width               0
+          ]
 
 
 drawNose :: Texture px
